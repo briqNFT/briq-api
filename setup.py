b1fac7459dd266a1cab1a7406346572490628f2d
Lancelot de Ferrière <wraitii@gmail.com>
2022-01-13 15:29:37 +0100

    sync version to frontend

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name='briq_api',
-      version='0.1',
+setup(
+      name='briq_api',
+      version='0.4',
       description='API for briq',
       url='http://github.com/S-L-Technologies/briq-api',
       author='briq',
       author_email='founders@sltech.company',
       license='MIT',
       packages=['briq_api'],
-      zip_safe=False)
+      zip_safe=False
+)
