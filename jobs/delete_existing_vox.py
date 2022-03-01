a22a8d26ed8fe6ded20e5df6563350c87524b70f
Lancelot de Ferrière <wraitii@gmail.com>
2022-03-01 14:42:18 +0100

    Vox deletion job

---
+++ jobs/delete_existing_vox.py
@@ -0,0 +1,27 @@
+## Objectives:
+# Delete all stored .vox, as they are broken.
+
+import copy
+import logging
+from briq_api.storage.cloud_storage import CloudStorage
+
+logger = logging.getLogger(__name__)
+
+if __name__ != '__main__':
+    logger.error("Must be called as a script")
+
+
+from briq_api.storage.storage import get_storage
+set_storage = get_storage()
+state_storate = get_storage("jobs/delete_existing_vox/")
+
+
+def delete_vox():
+    files = set_storage.iterate_files()
+    for file in files:
+        if file.endswith(".vox"):
+            if isinstance(set_storage, CloudStorage):
+                set_storage.bucket.blob(set_storage.path + file).delete()
+                logger.info("Deleting %(file)s", {"file": file})
+
+delete_vox()
