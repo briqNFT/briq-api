ed06777ed58e8c0c046852b9594cb7e572dbc29b
Lancelot de Ferrière <wraitii@gmail.com>
2022-06-13 16:42:10 +0200

    mock chain for genesis

--- briq_api/mock_chain/router.py
+++ briq_api/mock_chain/router.py
@@ -1,5 +1,6 @@
 import logging
 from typing import List
+from unittest import mock
 
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
@@ -11,6 +12,21 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
+mock_user = {
+    "available_boxes": [int("0xb0a001", 16), int("0xb0a002", 16)],
+    "opened_boxes": [],
+}
+
+BOX_CONTRACT = 0xD
+
+class getUnopenedBoxesCall(BaseModel):
+    owner: str
+
+
+@router.post("/getUnopenedBoxes")
+async def getUnopenedBoxes(body: getUnopenedBoxesCall):
+    return mock_user['available_boxes']
+
 
 class ContractCall(BaseModel):
     calldata: List[str]
@@ -18,8 +34,11 @@ class ContractCall(BaseModel):
     entry_point_selector: str
     signature: List[str]
 
+
+
 @router.post("/feeder_gateway/call_contract")
 async def call_contract(body: ContractCall):
+    print(body)
 
     if body.contract_address == '0xA':
         # briq contract
@@ -45,6 +64,18 @@ async def call_contract(body: ContractCall):
 
 @router.post("/gateway/add_transaction")
 async def add_transaction(body: ContractCall):
+    nb_calls = body.calldata[0]
+    contract = int(body.calldata[1])
+    selector = int(body.calldata[2])
+    args_len = int(body.calldata[5])
+    args = [int(x) for x in body.calldata[6:6 + args_len]]
+    nonce = body.calldata[-1]
+    # print(hex(contract), hex(selector), args, nonce)
+
+    if contract == BOX_CONTRACT and selector == get_selector_from_name('unbox_'):
+        mock_user['available_boxes'].remove(args[1])
+        mock_user['opened_boxes'].append(args[1])
+
     return {'transaction_hash': '0xfadedead'}
 
 
