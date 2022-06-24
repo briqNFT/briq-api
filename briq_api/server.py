ed7422d3a501064c877942760eefd34bd7036964
Lancelot de Ferrière <wraitii@gmail.com>
2022-06-24 16:49:29 +0200

    logging

--- briq_api/server.py
+++ briq_api/server.py
@@ -40,13 +40,16 @@ def health():
 @app.on_event("startup")
 def startup_event():
     if not os.getenv("LOCAL"):
+        logger.info("Connecting normally.")
         storage_client.connect_for_chain(chain_id="starknet-testnet", backend=CloudStorage('briq-bucket-test-1'))
         storage_client.connect_for_chain(legacy_chain_id, LegacyCloudStorage())
     else:
         # Don't attempt connecting to the cloud in that mode,
         # we expect to run locally and it makes it faster to reload the API
+        logger.info("Connecting locally.")
         storage_client.connect(FileStorage())
     if os.getenv("USE_MOCK_CHAIN"):
+        logger.info("Connecting for mock chain.")
         mock_storage = FileStorage()
         # Add an artificial slowdown
         mock_storage.slowdown = 0.5
