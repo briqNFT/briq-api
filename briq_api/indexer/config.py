ff1c38d2b780944c910ae2a389789ebe05c511ff
Lancelot de Ferrière <wraitii@gmail.com>
2022-08-15 11:22:01 +0200

    Configure mongo on K8S, and run runner/indexer on top of it

--- briq_api/indexer/config.py
+++ briq_api/indexer/config.py
@@ -1,6 +1,12 @@
+import os
 
-INDEXER_ID = "testnet-test-0"
+INDEXER_ID = os.getenv("INDEXER_ID", "testnet-test-0")
 # TODO: proper secret management
-MONGO_URL = "localhost:27017"
-MONGO_USERNAME = "apibara"
-MONGO_PASSWORD = "apibara"
+MONGO_URL = os.getenv("MONGO_URL", "localhost:27017")
+MONGO_USERNAME = os.getenv("MONGO_USERNAME", "apibara")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "apibara")
+
+NETWORK_NAME = os.getenv("NETWORK_NAME", "starknet-goerli")
+START_BLOCK = int(os.getenv("START_BLOCK", 285_000))
+
+APIBARA_URL = os.getenv("APIBARA_URL", None)
--- briq_api/indexer/runner.py
+++ briq_api/indexer/runner.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 from apibara import Client, IndexerRunner, Info, NewBlock, NewEvents
 from apibara.indexer.runner import IndexerRunnerConfiguration
 
-from .config import INDEXER_ID, MONGO_URL, MONGO_USERNAME, MONGO_PASSWORD
+from .config import INDEXER_ID, APIBARA_URL, MONGO_URL, MONGO_USERNAME, MONGO_PASSWORD, NETWORK_NAME, START_BLOCK
 from .events.bids import process_bids, bid_filter
 from .events.box import process_transfers as process_box, transfer_filters as box_filters
 from .events.booklet import process_transfers as process_booklet, transfer_filters as booklet_filters
@@ -14,13 +14,13 @@ from .events.briq import process_transfers as process_briq, transfer_filters as 
 
 logger = logging.getLogger(__name__)
 
-NETWORK_NAME = "starknet-goerli"
-START_BLOCK = 285_000
-
 
 async def handle_events(info: Info, block_events: NewEvents):
     block_time = block_events.block.timestamp
-    print(f"Handle block events: Block No. {block_events.block.number} - {block_time}")
+    logger.info("Handle block events: Block No. %(block_number)s - %(block_time)s", {
+        'block_number': block_events.block.number,
+        'block_time': block_time.isoformat()
+    })
 
     bids = process_bids(info, block_events.block, [event for event in block_events.events if event.name == 'Bid'])
     boxes = process_box(info, block_events.block, [event for event in block_events.events])
@@ -40,6 +40,7 @@ async def handle_block(info: Info, block: NewBlock):
         "timestamp": block.new_head.timestamp.isoformat(),
     }
     await info.storage.insert_one("blocks", block)
+    logger.info("Received block %(number)s", {'number': block['number']})
 
 # TODO: handle reorg
 
@@ -54,10 +55,11 @@ async def main(args):
             existing = await client.indexer_client().get_indexer(INDEXER_ID)
             if existing:
                 await client.indexer_client().delete_indexer(INDEXER_ID)
-                print("Indexer deleted. Starting from beginning.")
+                logging.warn("Indexer deleted. Starting from beginning.")
 
     runner = IndexerRunner(
         config=IndexerRunnerConfiguration(
+            apibara_url=APIBARA_URL,
             storage_url=f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_URL}"
         ),
         network_name=NETWORK_NAME,
