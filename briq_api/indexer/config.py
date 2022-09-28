6198b1cbb648adf0245eb63474b08863179683e0
Lancelot de Ferrière <wraitii@gmail.com>
2022-09-28 16:46:53 +0200

    Update infra for new indexer

--- briq_api/indexer/config.py
+++ briq_api/indexer/config.py
@@ -1,12 +1,12 @@
 import os
 
-INDEXER_ID = os.getenv("INDEXER_ID", "testnet-test-1")
+INDEXER_ID = os.getenv("INDEXER_ID", "testnet-test-2")
 # TODO: proper secret management
 MONGO_URL = os.getenv("MONGO_URL", "localhost:27017")
 MONGO_USERNAME = os.getenv("MONGO_USERNAME", "apibara")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "apibara")
 
 NETWORK_NAME = os.getenv("NETWORK_NAME", "starknet-goerli")
-START_BLOCK = int(os.getenv("START_BLOCK", 285_000))
+START_BLOCK = int(os.getenv("START_BLOCK", 352_600))
 
 APIBARA_URL = os.getenv("APIBARA_URL", "goerli.starknet.stream.apibara.com")
