4e2558ec0a80ea3b80d9c4f9d9b3bdf66c1840da
Lancelot de Ferrière <wraitii@gmail.com>
2022-12-13 16:40:16 +0100

    WIP airdrop script to get addresses

---
+++ jobs/briqmas_airdrop_list.py
@@ -0,0 +1,49 @@
+from briq_api.indexer.storage import MongoBackend, MongoStorage
+
+mongo = MongoBackend(db_name="kub_mainnet_2")
+
+owners = set()
+
+tokens = mongo.db['box_tokens'].find({
+    "$or": [
+        { "_chain.valid_to": { "$gte": 8000, "$lte": 10000 } },
+        { "_chain.valid_to": None },
+    ]
+})
+
+for data in tokens:
+    print("box -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'])
+    owners.add(int.from_bytes(data['owner'], "big"))
+
+tokens = mongo.db['booklet_tokens'].find({
+    "$or": [
+        { "_chain.valid_to": { "$gte": 8000, "$lte": 10000 } },
+        { "_chain.valid_to": None },
+    ]
+})
+
+for data in tokens:
+    print("booklet -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'])
+    owners.add(int.from_bytes(data['owner'], "big"))
+
+
+tokens = mongo.db['set_tokens'].find({
+    "$or": [
+        { "_chain.valid_to": { "$gte": 8000, "$lte": 10000 } },
+        { "_chain.valid_to": None },
+    ]
+})
+
+sets = set()
+for data in tokens:
+    sets.add(int.from_bytes(data['token_id'], "big"))
+    booklets = mongo.db['booklet_tokens'].find({ "owner": data['token_id'] })
+    if len(list(booklets)) > 0:
+        print("set -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'], int.from_bytes(data['token_id'], "big"))
+        owners.add(int.from_bytes(data['owner'], "big"))
+    else:
+        print("noneligible set -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'], int.from_bytes(data['token_id'], "big"))
+
+airdrop_recipients = owners.difference(sets)
+for owner in airdrop_recipients:
+    print(hex(owner))
