bc0159c3406c3b8822b5dc518088de9a7125defc
Lancelot de Ferrière <wraitii@gmail.com>
2022-12-14 16:08:44 +0100

    fix airdrop script to account for quantity

--- jobs/briqmas_airdrop_list.py
+++ jobs/briqmas_airdrop_list.py
@@ -5,6 +5,7 @@ mongo = MongoBackend(db_name="kub_mainnet_2")
 owners = set()
 
 tokens = mongo.db['box_tokens'].find({
+    "quantity": { "$ne": (0).to_bytes(32, "big") },
     "$or": [
         { "_chain.valid_to": { "$gte": 8000, "$lte": 10000 } },
         { "_chain.valid_to": None },
@@ -12,10 +13,11 @@ tokens = mongo.db['box_tokens'].find({
 })
 
 for data in tokens:
-    print("box -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'])
+    print("box -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'], data['_chain']['valid_from'])
     owners.add(int.from_bytes(data['owner'], "big"))
 
 tokens = mongo.db['booklet_tokens'].find({
+    "quantity": { "$ne": (0).to_bytes(32, "big") },
     "$or": [
         { "_chain.valid_to": { "$gte": 8000, "$lte": 10000 } },
         { "_chain.valid_to": None },
@@ -23,11 +25,12 @@ tokens = mongo.db['booklet_tokens'].find({
 })
 
 for data in tokens:
-    print("booklet -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'])
+    print("booklet -> ", hex(int.from_bytes(data['owner'], "big")), hex(int.from_bytes(data['token_id'], "big")), data['_chain']['valid_to'], data['_chain']['valid_from'])
     owners.add(int.from_bytes(data['owner'], "big"))
 
 
 tokens = mongo.db['set_tokens'].find({
+    "quantity": { "$ne": (0).to_bytes(32, "big") },
     "$or": [
         { "_chain.valid_to": { "$gte": 8000, "$lte": 10000 } },
         { "_chain.valid_to": None },
