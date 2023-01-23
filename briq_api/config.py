0defa48b6ff5a15423a9ef5024680b9a2f8de130
Lancelot de Ferrière <wraitii@gmail.com>
2023-01-23 11:06:14 +0100

    improve typing a bit

--- briq_api/config.py
+++ briq_api/config.py
@@ -1,4 +1,8 @@
 import os
+from typing import Literal, NewType, cast
 
-ENV = os.getenv("ENV") or "dev"
+ENV_VAR = Literal["dev", "test", "prod"]
+
+ENV: ENV_VAR
+ENV = cast(ENV_VAR, os.getenv("ENV")) or "dev"
 assert ENV in ["dev", "test", "prod"]
