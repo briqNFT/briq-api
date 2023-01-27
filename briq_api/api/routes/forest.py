c1a57f3d64aeef585f4503160509b3b1675837dd
Lancelot de Ferrière <wraitii@gmail.com>
2023-01-27 18:17:16 +0100

    Minor optimisation to briq forest

--- briq_api/api/routes/forest.py
+++ briq_api/api/routes/forest.py
@@ -12,9 +12,21 @@ logger = logging.getLogger(__name__)
 router = APIRouter(route_class=ExceptionWrapperRoute(logger))
 
 
+forest_data = {
+    "last_update": 0,
+    "data": {}
+}
+
+
 @router.head("/forest/{chain_id}/full_data")
 @router.get("/forest/{chain_id}/full_data")
 async def get_forest_data(chain_id: str):
+    print(datetime.now().timestamp(), forest_data["last_update"] + 5 * 60)
+    if datetime.now().timestamp() < forest_data["last_update"] + 5 * 60:
+        return JSONResponse(forest_data["data"], headers={
+            "Cache-Control": f"public, max-age={5*60}"
+        })
+
     trees = mongo_storage.get_backend(chain_id).db["booklet_tokens"].find({
         "token_id": ((2**192 * 10) + 1).to_bytes(32, "big"),
         "_chain.valid_to": None,
@@ -36,7 +48,8 @@ async def get_forest_data(chain_id: str):
             "age": round(age),
             "owner": hex(int.from_bytes(actual_set["owner"], "big")),
         }
-
+    forest_data["data"] = out
+    forest_data["last_update"] = datetime.now().timestamp()
     return JSONResponse(out, headers={
         "Cache-Control": f"public, max-age={5*60}"
     })
