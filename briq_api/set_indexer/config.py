3d6d8bec220db2164c9137615a035fc6e88d043f
Lancelot de Ferrière <wraitii@gmail.com>
2023-09-11 17:58:24 +0200

    Update sidecar, default set-indexer to testnet-dojo

--- briq_api/set_indexer/config.py
+++ briq_api/set_indexer/config.py
@@ -1,4 +1,4 @@
 import os
 from briq_api.chain.networks import get_network_metadata
 
-NETWORK = get_network_metadata(os.getenv("NETWORK_NAME") or "starknet-testnet")
+NETWORK = get_network_metadata(os.getenv("NETWORK_NAME") or "starknet-testnet-dojo")
